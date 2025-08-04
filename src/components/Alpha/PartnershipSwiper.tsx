e0fb966f13b6c0d79e6974161f82dee7e3397587
Axgust22 <cheangseyha2208@gmail.com>
2025-08-04 13:53:39 +0700

    feat: add PartnershipSlider component and update Project layout with new logos

---
+++ src/components/Alpha/PartnershipSwiper.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import Image from "next/image";
+
+const partners = [
+  { name: "Aba", logo: "/Slide/ABA-Logo.webp" },
+  { name: "BD_logo", logo: "/Slide/bd-logo.png" },
+  { name: "Chipmong", logo: "/Slide/Chipmong-logo.png" },
+  { name: "Woori Bank", logo: "/Slide/woori-logo.webp" },
+];
+
+export default function PartnershipSlider() {
+  return (
+    <div className="w-full overflow-hidden">
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        loop={true}
+        speed={5000} // Control speed (higher = slower)
+        autoplay={{
+          delay: 0, // No delay, continuous movement
+          disableOnInteraction: false,
+        }}
+        grabCursor={false}
+        modules={[Autoplay]}
+        breakpoints={{
+          640: { slidesPerView: 3 },
+          768: { slidesPerView: 4 },
+          1024: { slidesPerView: 5 },
+        }}
+      >
+        {partners.concat(partners).map((partner, index) => (
+          <SwiperSlide key={index} className="flex justify-center items-center">
+            <Image
+              src={partner.logo}
+              alt={partner.name}
+              width={200}
+              height={200}
+              className="w-40 h-40 object-contain"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
