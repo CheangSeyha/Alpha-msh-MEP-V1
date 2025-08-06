7570ebf7c006c6ce6e52a96d05a2df28e67b04e4
Axgust22 <cheangseyha2208@gmail.com>
2025-08-06 14:38:14 +0700

    feat: add new components for team and MEP carousels, mission section, and implement outside click hook

--- src/app/[locale]/MVC/page.tsx
+++ src/app/[locale]/MVC/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+import Footer from "@/components/Alpha/Footer";
+import Mission from "@/components/Alpha/Mission";
+import Navbar from "@/components/Alpha/Navbar";
+import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 function page() {
-  return <div>mission vision & core values</div>;
+  const t = useTranslations("MVC");
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="z-50">
+        <Navbar />
+      </header>
+      <main className="flex flex-col mt-30 lg:mt-50 ">
+        <section className="w-full mx-auto">
+          <div className="relative w-full mx-auto">
+            <motion.img
+              src="/cambodia.jpg"
+              alt="cambodia"
+              className="object-cover w-full h-[50%]"
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1.5, ease: "easeOut" }}
+            />
+            <motion.h1
+              className="absolute top-0 left-1/2 transform -translate-x-1/2 text-center font-exo text-sm md:text-2xl lg:text-4xl font-bold text-black uppercase p-2 md:p-5"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1, duration: 1 }}
+            >
+              {t("mission&vision")}
+            </motion.h1>
+          </div>
+        </section>
+
+        <section className="w-full max-w-6xl mx-auto px-4 md:px-6 lg:px-8 xl:px-0 mt-10">
+          <Mission />
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default page;
--- src/app/[locale]/Team/page.tsx
+++ src/app/[locale]/Team/page.tsx
@@ -1,7 +1,44 @@
+"use client";
+import { MEPCarousel } from "@/components/Alpha/MEPCarousel";
+import { CivilandArchitectureCarousel } from "@/components/Alpha/CivilandArchitectureCarousel";
+import Footer from "@/components/Alpha/Footer";
+import Navbar from "@/components/Alpha/Navbar";
+import { useTranslations } from "next-intl";
 import React from "react";
+import { OfficeCarousel } from "@/components/Alpha/OfficeCarousel";
 
 function page() {
-  return <div>team</div>;
+  const t = useTranslations("Team");
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="z-50">
+        <Navbar />
+      </header>
+      <main className="flex flex-col mt-30 lg:mt-50">
+        <section className="w-full mx-auto">
+          <div className="relative w-full mx-auto">
+            <img
+              src="/cambodia.jpg"
+              alt="cambodia"
+              className="object-cover w-full h-[%50]"
+            />
+            <h1 className="absolute top-0 left-1/2 transform -translate-x-1/2 text-center font-poppins text-sm md:text-2xl lg:text-4xl font-bold text-black uppercase p-2 md:p-5">
+              {t("team")}
+            </h1>
+          </div>
+        </section>
+
+        <section className="w-full max-w-6xl mx-auto px-4 md:px-6 lg:px-8 xl:px-0">
+          <MEPCarousel />
+          <CivilandArchitectureCarousel />
+          <OfficeCarousel />
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default page;
